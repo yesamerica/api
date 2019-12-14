e4ceec59757e64b007c465e46f948a5fea7d1a48
roman-jordan <rjordan1352@gmail.com>
2019-12-13 21:24:35 -0800

    yo

--- data/seeds/002-roles.js
+++ data/seeds/002-roles.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('roles').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('roles').insert([
         {role: 'individual'},
         {role: 'consumer'},
         {role: 'provider'},
--- data/seeds/003-userroles.js
+++ data/seeds/003-userroles.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('user_roles').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('user_roles').insert([
         { user_id:1,role_id:1},
         { user_id:2,role_id:2},
         { user_id:3,role_id:4}
