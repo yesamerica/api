6d22d4ca7a076724cb9cfacdd8edf8ab2df01407
roman-jordan <rjordan1352@gmail.com>
2019-12-14 18:16:38 -0800

    Users Test

--- api/private/server.js
+++ api/private/server.js
@@ -1,3 +1,5 @@
 const privateRouter = require('express').Router()
-
+const usersRouter = require('./users/users')
+const jwt = require(_jwt)
+privateRouter.use('/users',usersRouter)
 module.exports=privateRouter
--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ const authRouter = require("./auth/auth")
 //Implement Routes
 primaryRouter.use('/',authRouter)
 primaryRouter.use('/public',publicRouter)
-primaryRouter.use('/private',jwt.chkToken(),privateRouter)
+primaryRouter.use('/',jwt.chkToken(),privateRouter)
 
 primaryRouter.use('/privacy',(req,res)=>{
     res.status(200).json({message:"Zero liability policy"});
